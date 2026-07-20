d5e4c766bebc4045fac184ec730640a408baf78d
ghy93 <gonghy8@mail2.sysu.edu.cn>
2026-07-20 15:25:24 +0800

    Automated commit from agent

--- agent/__init__.py
+++ agent/__init__.py
@@ -3,11 +3,13 @@ from .robostate_agent import RoboStateAgent
 from .saycan_agent import SayCanAgent
 from .llm_planner_agent import LLMPlannerAgent
 from .base_agent import BaseAgent
+from .flare_agent import FLAREAgent
 
 AGENT_REGISTRY = {
     'robostate': RoboStateAgent,
     'saycan': SayCanAgent,
-    'llm_planner': LLMPlannerAgent
+    'llm_planner': LLMPlannerAgent,
+    'flare': FLAREAgent,
 }
 
-__all__ = ["AGENT_REGISTRY", "BaseAgent", "RoboStateAgent", "SayCanAgent", "LLMPlannerAgent"]
+__all__ = ["AGENT_REGISTRY", "BaseAgent", "RoboStateAgent", "SayCanAgent", "LLMPlannerAgent","FLAREAgent"]
